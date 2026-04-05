b6ffaf544265f9fd3bdb3b86eeb6fd807db9b99a
anandversespace-debug <anandverse.space@gmail.com>
2026-04-05 19:57:43 +0530

    fix(cors): allow requests from www.foodagriexpo.com platform

--- src/app.js
+++ src/app.js
@@ -30,7 +30,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: [
+        process.env.CLIENT_URL,
+        'http://localhost:5173',
+        'https://www.foodagriexpo.com',
+        'https://foodagriexpo.com'
+    ].filter(Boolean),
     credentials: true
 }));
 
